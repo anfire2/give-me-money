b3cd13595e7b01f8c5fe10c57c7c31e1903aa031
anyan <anyan@eccom.com.cn>
2021-12-06 15:19:25 +0800

    1206更新：1.修复输入不支持小数的问题，修复重复计算导致输出相同结果的问题，略微增强对输入数字的校验；2.调整输入界面至三列，微调标题样式；3.除控制台外，增加界面右侧输出显示

--- src/cacluator/Home.js
+++ src/cacluator/Home.js
@@ -1,37 +1,51 @@
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 import { calculateResult } from './util';
-import { Button, Form, Input } from 'antd';
+import { Button, Form, Input, Tooltip, Typography } from 'antd';
 
+const { Title, Text } = Typography;
 
 const Home = () => {
+
+  const [inputListForPrint, setInputListForPrint] = useState([]);
+  const [targetValueForPrint, setTargetValueForPrint] = useState(0);
+  const [resultListForPrint, setResultListForPrint] = useState([]);
+
 
   const calculator = useCallback((inputList, targetValue) => {
     return calculateResult(inputList, targetValue);
   }, []);
 
-
   const submit = (values) => {
     const { targetValue, ...restInput } = values;
-    const inputList = Object.values(restInput).filter(v => !!v).map(k => parseInt(k, 10));
+    const inputList = Object.values(restInput).filter(v => !!v).map(k => parseFloat(k));
 
-    const resultList = calculator(inputList, targetValue);
+    if (inputList.length) {
+      const resultList = calculator(inputList, targetValue);
 
+      console.log('发票金额：' + inputList);
+      console.log('目标额度：' + targetValue + '元');
 
-    console.log('发票金额：' + inputList);
-    console.log('目标额度：' + targetValue + '元');
-    console.log(resultList);
-    resultList.forEach(v => {
-      console.log('最接近目标值的组合为 ' + JSON.stringify(v.list) + ' ,合计' + v.total + '元');
-    });
+      resultList.forEach(v => {
+        console.log('最接近目标值的组合为 ' + JSON.stringify(v.list) + ' ,合计' + v.total + '元');
+      });
+
+      setInputListForPrint(inputList);
+      setTargetValueForPrint(targetValue);
+      setResultListForPrint(resultList);
+    }else{
+      setInputListForPrint([]);
+      setTargetValueForPrint(0);
+      setResultListForPrint([]);
+    }
   };
 
   const initForm = () => {
     const formList = [];
-    for (let i = 0; i < 20; i += 1) {
+    for (let i = 0; i < 21; i += 1) {
       formList.push(
         <Form.Item
-          label={'Value_' + i}
           name={'value_' + i}
+          style={{ display: 'inline-block', width: '33%' }}
         >
           <Input />
         </Form.Item>
@@ -41,35 +55,53 @@ const Home = () => {
   };
 
   return (
-    <div style={{ width: '50%' }}>
-      <Form
-        name="basic"
-        labelCol={{
-          span: 8,
-        }}
-        wrapperCol={{
-          span: 16,
-        }}
-        onFinish={submit}
-        autoComplete="off"
-        style={{ margin: 50 }}
+    <>
+      <Text
+        style={{ margin: 10 }}
       >
-        <Form.Item label="目标额度" name="targetValue">
-          <Input />
-        </Form.Item>
-        {initForm()}
-        <Form.Item
+        <Tooltip title="1206更新：1.修复输入不支持小数的问题，修复重复计算导致输出相同结果的问题，略微增强对输入数字的校验；2.调整输入界面至三列，微调标题样式；3.除控制台外，增加界面右侧输出显示">
+          发票助手-不高于（交通费）V 0.9.丐
+        </Tooltip>
+      </Text>
+      <div style={{ display: 'flex' }}>
+        <Form
+          name="basic"
           wrapperCol={{
-            offset: 8,
-            span: 16,
+            span: 18,
           }}
+          onFinish={submit}
+          autoComplete="off"
+          style={{ margin: '4%', width: '50%' }}
         >
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
-    </div>
+          <Title level={3}>
+            目标额度
+          </Title>
+          <Form.Item name="targetValue">
+            <Input />
+          </Form.Item>
+          <Title level={4}>
+            输入发票金额
+          </Title>
+          {initForm()}
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              提交
+            </Button>
+          </Form.Item>
+        </Form>
+        {resultListForPrint?.length ? (
+          <div style={{ margin: '4%', width: '50%' }}>
+            <Title level={4}>发票金额：{JSON.stringify(inputListForPrint, null, 2)}</Title>
+            <br />
+            <Title level={4}>目标额度：{targetValueForPrint} 元</Title>
+            <br />
+            {resultListForPrint.map((v) =>
+              (<Title level={4}>最接近目标值的组合为：{JSON.stringify(v.list, null, 2)}，合计 {v.total} 元</Title>)
+            )}
+          </div>
+        ) : null}
+      </div>
+    </>
   );
 };
 
--- src/cacluator/util.js
+++ src/cacluator/util.js
@@ -1,42 +1,45 @@
-export function calculateResult(inputList,targetValue){
-    let maxList=[]
-    let depth = inputList.length
-    let tempPath = [];
-    let currentMax = 0;
+export function calculateResult(inputList, targetValue) {
+  let maxList = [];
+  let depth = inputList.length;
+  let tempPath = [];
+  let currentMax = 0;
 
-    const search=(i)=> { // 表示递归深度
-        if (i >= depth) {
-            checkMax(); //检查最大值
-        }
-        else {
-            tempPath[i] = 0;
-            search(i + 1);
-            tempPath[i] = 1;
-            search(i + 1);
-        }
+  const search = (i) => { // 表示递归深度
+    if (i >= depth) {
+      checkMax(); //检查最大值
+    } else {
+      tempPath[i] = 0;
+      search(i + 1);
+      tempPath[i] = 1;
+      search(i + 1);
     }
+  };
 
-    const checkMax = ()=>{
-        let value = 0;
-        for (let i = 0; i < depth; i++) {
-            if (tempPath[i] === 1) {
-                value += inputList[i];
-            }
-        }
-        if (value <= targetValue) { // 判断是否达到上限
-            let list = []
-            tempPath.forEach((p, index) => { if (p === 1) { list.push(inputList[index]) } })
-            if (value > currentMax) {
-                maxList = [{ total: value, list }]
-                currentMax = value
-            }
-            if (value === currentMax) {
-                maxList.push({ total: value, list })
-            }
-        }
+  const checkMax = () => {
+    let value = 0;
+    for (let i = 0; i < depth; i++) {
+      if (tempPath[i] === 1) {
+        value += inputList[i];
+      }
     }
+    if (value <= targetValue) { // 判断是否达到上限
+      let list = [];
+      tempPath.forEach((p, index) => {
+        if (p === 1) {
+          list.push(inputList[index]);
+        }
+      });
+      if (value > currentMax) {
+        maxList = [{ total: value, list }];
+        currentMax = value;
+      }
+      if (value === currentMax && list.sort().toString() !== maxList[0]?.list.sort().toString()) {
+        maxList.push({ total: value, list });
+      }
+    }
+  };
 
-    search(0);
-    return maxList
+  search(0);
+  return maxList;
 }
 
