8e4ad729f0927373aa0247e22de99440fe0530f9
anyan <anyan@eccom.com.cn>
2023-04-19 15:54:06 +0800

    **2023.04.19**
    *V 0.9.7*
    
    - 更名为Eccom Money
    - 去除【注】部分的内容，去除左上角的版本和更新提示，标题移动至右下角
    - 优化了一下边距排版
    - 更新相关依赖至最新

--- src/cacluator/Home.js
+++ src/cacluator/Home.js
@@ -1,9 +1,9 @@
 import { useCallback, useState } from 'react';
 import { calculateResult } from './util';
-import { Button, Form, Input, Popover, Radio, Space, Tag, Tooltip, Typography } from 'antd';
+import { Button, Form, Input, Popover, Radio, Tag, Typography } from 'antd';
 import qrcode from './qr_code.webp';
 
-const { Title, Text } = Typography;
+const { Title } = Typography;
 
 const Home = () => {
 
@@ -51,7 +51,7 @@ const Home = () => {
           name={'value_' + i}
           style={{ display: 'inline-block', width: '33%' }}
         >
-          <Input allowClear/>
+          <Input allowClear />
         </Form.Item>
       );
     }
@@ -60,14 +60,7 @@ const Home = () => {
 
   return (
     <>
-      <Text
-        style={{ margin: 10 }}
-      >
-        <Tooltip title="20220628：1.发票金额输入框支持单独清空；2.经人肉测试报销发票金额超过费用额度时也会被退回，因此报销方式不再对“额度券”和“费用额度”进行区分建议">
-          E票M - 发票助手 V 0.9.6
-        </Tooltip>
-      </Text>
-      <div style={{ display: 'flex' }}>
+      <div style={{ display: 'flex', padding: '0 2%' }}>
         <Form
           name="basic"
           wrapperCol={{
@@ -75,7 +68,7 @@ const Home = () => {
           }}
           onFinish={submit}
           autoComplete="off"
-          style={{ padding: '2% 4%', width: '50%' }}
+          style={{ padding: '2% 4% 0', width: '50%' }}
         >
           <Title level={4}>
             计算方式
@@ -106,7 +99,7 @@ const Home = () => {
           </Form.Item>
         </Form>
         {inputListForPrint?.length ? (
-          <div style={{ padding: '2% 4%', width: '50%' }}>
+          <div style={{ padding: '2% 4% 0', width: '50%' }}>
             <Title level={4}>发票金额：</Title>
             <Title level={5}>{JSON.stringify(inputListForPrint, null, 2)}</Title>
             <br />
@@ -121,20 +114,16 @@ const Home = () => {
           </div>
         ) : null}
       </div>
-      <div style={{ paddingLeft: '4%' }}>
-        <Space direction="vertical">
-          <span>注：</span>
-          <span>
-            选择【相加不超过】或【相加不低于】时，均将计算出最接近目标额度的组合
-          </span>
-          <span>
-            报销要求发票金额必须与报销金额【完全一致】，否则可能会被退回
-          </span>
-        </Space>
-      </div>
       <div
         style={{
-          position: 'fixed', right: 40, bottom: 40, zIndex: 9999, display: 'flex', cursor: 'pointer'
+          position: 'fixed',
+          right: 28,
+          bottom: 24,
+          zIndex: 9999,
+          display: 'flex',
+          flexDirection: 'column',
+          cursor: 'pointer',
+          alignItems: 'center'
         }}
       >
         <Popover
@@ -143,6 +132,9 @@ const Home = () => {
         >
           <Tag color="#108ee9">→ 点赞/吐槽/需求/BUG ←</Tag>
         </Popover>
+        <Title level={5}>
+          Eccom Money
+        </Title>
       </div>
     </>
   );
--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import 'antd/dist/reset.css';
+import './index.css';
 import Home from './cacluator/Home';
-import 'antd/dist/antd.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const root = createRoot(document.getElementById("root"));
+root.render(<Home />);
